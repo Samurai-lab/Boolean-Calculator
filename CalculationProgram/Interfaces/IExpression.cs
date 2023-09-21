b3d7a52144c3a37e7f6c65ffc7617ef600d22015
Egor <brazkhin.e92@gmail.com>
2023-09-21 22:56:16 +0500

    Add new function in Expression class & code refactor

--- CalculationProgram/Interfaces/IExpression.cs
+++ CalculationProgram/Interfaces/IExpression.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CalculationProgram.Interfaces;
 
 namespace CalculationProgram
 {
     public interface IExpression
     { 
-       public void UseExpression();
+       public void UseExpression(IUniversum  universum);
     }
 }
